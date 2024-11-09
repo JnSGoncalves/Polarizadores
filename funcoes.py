c94d0ac48cfb67cd86d327c051c44068c6356f66
wallace0003 <wallaceizidoro.1@gmail.com>
2024-11-09 10:31:38 -0300

    mudando a main e algumas funcoes

--- funcoes.py
+++ funcoes.py
@@ -26,16 +26,16 @@ def tresPolarizadores(posIni, intensidade, an1, an2, an3):
         return resul1, resul2, resulFinal
 
 def doisPolarizadores(posInicial, intensidade, ang1, ang2):
-    if(posInicial == 1):
+    if(posInicial == 0):
         i2 = intensidade / 2
-        i3 = i2 * cos(ang2)**2
+        i3 = i2 * math.cos(math.radians(ang2))**2
         return i2, i3 
-    elif (posInicial == 2):
+    elif (posInicial == 1):
         i1 = intensidade * 2
-        i3 = intensidade * cos(ang2 - ang1)**2
+        i3 = intensidade * math.cos(math.radiasn(ang2 - ang1))**2
         return i1, i3
-    elif (posInicial == 3):
-        i2 = intensidade / cos(ang1 - ang2)**2
+    elif (posInicial == 2):
+        i2 = intensidade / math.cos(math.radians(ang1 - ang2))**2
         i1 = i2 * 2
         return i1, i2
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from funcoes import *
+
 print("""Autores:
 
 Jônatas da Silva Gonçalves.
@@ -6,9 +8,16 @@ Pedro Henrique da Fonseca do Nascimento.
 Vinícius do Nascimento Generoso.\n""")
 
 print("Calculadora Ótica-python\n")
-
+print()
 print("Caso deseje entrar com valores em notação científica, utilize o formato abaixo:")
 print("1.23 x 10^4 --> 1.23e4\n")
+print()
+
+print("Atenção para posição inicial.")
+print("Digite 0 -> caso não tenha incidido nenhum polarizador")
+print("Digite 1. Se incidiu apenas 1 polarizador")
+print("Digite 2. Caso tenha incindido em 2 polarizadores")
+print("Assim, sucessivamente.")
 
 
 while True:
@@ -23,12 +32,12 @@ while True:
     
     if(opcao == "1"):
         posInicial = int(input("Digite o valor da posicão inicial: "))
-        intensidade = float(input("Digite a intencidade: "))
-        ang1 = float(input("Digite o primeiro ângulo: "))
-        ang2 = float(input("Digite o segundo ângulo: "))
+        intensidade = float(input("Digite a intensidade: "))
+        ang1 = float(input("Digite o primeiro ângulo em graus: "))
+        ang2 = float(input("Digite o segundo ângulo em graus: "))
 
         if(posInicial == "1"):
-            saida1, saida2 = doisPolarizadores(posInicial, intensidadeInicial, ang1, ang2)
+            saida1, saida2 = doisPolarizadores(posInicial, intensidade, ang1, ang2)
         else:
             print("Entrada inválida.")
             print()
